21b67099f79067639184f05a97f36648dd1f3883
DasonCheng <dasoncheng@foxmail.com>
2017-09-02 21:43:58 +0800

    base ui

--- app/script/main.js
+++ app/script/main.js
@@ -1,10 +1,8 @@
-(() => {
-    class Test {
-        constructor(name) {
-            this.name = name
-        }
-        SayHello() {
-            console.log(`Hello!I'm ${this.name}`)
-        }
-    }
-})()
+let Dialog = async(el, options) => {
+  this.options = Object.assign({
+    title: "提示",
+    message: "确定执行此操作？",
+    ctrls: ["取消", "确定"],
+  }, options)
+
+}
